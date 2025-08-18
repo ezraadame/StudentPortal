2c849a4060dac0fcfba0a4820db6e604fd0817e8
Oscar Adame <oadame1@wgu.edu>
2025-08-18 14:53:46 -0500

    Add project files.

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace StudentPortal
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
