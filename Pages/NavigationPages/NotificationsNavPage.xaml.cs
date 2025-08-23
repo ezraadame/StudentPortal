0e292e47e37bd1fedc62dd5b74eb319ad95301e0
Oscar Adame <oadame1@wgu.edu>
2025-08-23 07:21:28 -0500

    Implemented notification alerts

--- Pages/NavigationPages/NotificationsNavPage.xaml.cs
+++ Pages/NavigationPages/NotificationsNavPage.xaml.cs
@@ -1,5 +1,6 @@
 using Plugin.LocalNotification;
 using StudentPortal.Models;
+using StudentPortal.Services;
 
 namespace StudentPortal.Pages.NavigationPage
 {
@@ -10,46 +11,178 @@ namespace StudentPortal.Pages.NavigationPage
             InitializeComponent();
         }
 
-        private void TermStartDateToggle_Toggled(object sender, ToggledEventArgs e)
+        protected override void OnAppearing()
         {
+            base.OnAppearing();
+            LoadToggleStates();
+        }
+
+        private void LoadToggleStates()
+        {
+            TermStartDateToggle.IsToggled = Preferences.Get("TermStartNotifications", false);
+            TermEndDateToggle.IsToggled = Preferences.Get("TermEndNotifications", false);
+            CourseStartDateToggle.IsToggled = Preferences.Get("CourseStartNotifications", false);
+            CourseEndDateToggle.IsToggled = Preferences.Get("CourseEndNotifications", false);
+            AssessmentStartDateToggle.IsToggled = Preferences.Get("AssessmentStartNotifications", false);
+            AssessmentEndDateToggle.IsToggled = Preferences.Get("AssessmentEndNotifications", false);
 
         }
 
-        private void TermEndDateToggle_Toggled(object sender, ToggledEventArgs e)
+        private async void TermStartDateToggle_Toggled(object sender, ToggledEventArgs e)
         {
-            var request = new NotificationRequest()
+            Preferences.Set("TermStartNotifications", e.Value);
+
+            var terms = await DBService.GetTerms();
+            var today = DateTime.Today;
+            var termsStartingToday = terms.Where(term => term.StartDate.Date == today).ToList();
+
+            foreach (var term in termsStartingToday)
             {
-                Title = "Term End Date Alert",
-                Subtitle = "Term ends soon",
-                BadgeNumber = 42,
-                CategoryType = NotificationCategoryType.Reminder,
-                Schedule = new NotificationRequestSchedule()
+                var request = new NotificationRequest()
                 {
-                    
-                }
-            };
-
-            LocalNotificationCenter.Current.Show(request);
+                    NotificationId = term.Id + 1000,
+                    Title = "TERM STARTING TODAY!",
+                    Description = $"Your term '{term.Name}' is starting today!",
+                    BadgeNumber = 1,
+                    CategoryType = NotificationCategoryType.Reminder,
+                    Schedule = new NotificationRequestSchedule()
+                    {
+                        NotifyTime = DateTime.Now.AddSeconds(5)
+                    }
+                };
+                await LocalNotificationCenter.Current.Show(request);
+            }
         }
 
-        private void CourseStartDateToggle_Toggled(object sender, ToggledEventArgs e)
+        private async void TermEndDateToggle_Toggled(object sender, ToggledEventArgs e)
         {
+            Preferences.Set("TermEndNotifications", e.Value);
 
+            var terms = await DBService.GetTerms();
+            var today = DateTime.Today;
+            var termsEndingToday = terms.Where(term => term.EndDate.Date == today).ToList();
+
+            foreach (var term in termsEndingToday)
+            {
+                var request = new NotificationRequest()
+                {
+                    NotificationId = term.Id + 2000,
+                    Title = "TERM ENDING TODAY",
+                    Description = $"Your term '{term.Name}' is ending today!",
+
+                    BadgeNumber = 42,
+                    CategoryType = NotificationCategoryType.Reminder,
+                    Schedule = new NotificationRequestSchedule()
+                    {
+                        NotifyTime = DateTime.Now.AddSeconds(5)
+                    }
+                };
+                await LocalNotificationCenter.Current.Show(request);
+            }
         }
 
-        private void CourseEndDateToggle_Toggled(object sender, ToggledEventArgs e)
+        private async void CourseStartDateToggle_Toggled(object sender, ToggledEventArgs e)
         {
+            Preferences.Set("CourseStartNotifications", e.Value);
 
+            var courses = await DBService.GetCourses();
+            var today = DateTime.Today;
+            var coursesStartingToday = courses.Where(course => course.StartDate.Date == today).ToList();
+
+            foreach (var course in coursesStartingToday)
+            {
+                var request = new NotificationRequest()
+                {
+                    NotificationId = course.Id + 3000,
+                    Title = "COURSE STARTING TODAY!",
+                    Description = $"Your term '{course.Name}' is starting today!",
+                    BadgeNumber = 1,
+                    CategoryType = NotificationCategoryType.Reminder,
+                    Schedule = new NotificationRequestSchedule()
+                    {
+                        NotifyTime = DateTime.Now.AddSeconds(5)
+                    }
+                };
+                await LocalNotificationCenter.Current.Show(request);
+            }
         }
 
-        private void AssessmentStartDateToggle_Toggled(object sender, ToggledEventArgs e)
+        private async void CourseEndDateToggle_Toggled(object sender, ToggledEventArgs e)
         {
+            Preferences.Set("CourseEndNotifications", e.Value);
 
+            var courses = await DBService.GetCourses();
+            var today = DateTime.Today;
+            var coursesEndingToday = courses.Where(course => course.EndDate.Date == today).ToList();
+
+            foreach (var course in coursesEndingToday)
+            {
+                var request = new NotificationRequest()
+                {
+                    NotificationId = course.Id + 4000,
+                    Title = "COURSE ENDING TODAY!",
+                    Description = $"Your term '{course.Name}' is ending today!",
+                    BadgeNumber = 1,
+                    CategoryType = NotificationCategoryType.Reminder,
+                    Schedule = new NotificationRequestSchedule()
+                    {
+                        NotifyTime = DateTime.Now.AddSeconds(5)
+                    }
+                };
+                await LocalNotificationCenter.Current.Show(request);
+            }
         }
 
-        private void AssessmentEndDateToggle_Toggled(object sender, ToggledEventArgs e)
+        private async void AssessmentStartDateToggle_Toggled(object sender, ToggledEventArgs e)
         {
+            Preferences.Set("AssessmentStartNotifications", e.Value);
 
+            var assessments = await DBService.GetAssessments();
+            var today = DateTime.Today;
+            var assessmentStartingToday = assessments.Where(assessment => assessment.StartDate.Date == today).ToList();
+
+            foreach (var assessment in assessmentStartingToday)
+            {
+                var request = new NotificationRequest()
+                {
+                    NotificationId = assessment.Id + 5000,
+                    Title = "ASSESSMENT STARTING TODAY!",
+                    Description = $"Your assessment '{assessment.Name}' is starting today!",
+                    BadgeNumber = 1,
+                    CategoryType = NotificationCategoryType.Reminder,
+                    Schedule = new NotificationRequestSchedule()
+                    {
+                        NotifyTime = DateTime.Now.AddSeconds(5)
+                    }
+                };
+                await LocalNotificationCenter.Current.Show(request);
+            }
+        }
+
+        private async void AssessmentEndDateToggle_Toggled(object sender, ToggledEventArgs e)
+        {
+            Preferences.Set("AssessmentEndNotifications", e.Value);
+
+            var assessments = await DBService.GetAssessments();
+            var today = DateTime.Today;
+            var assessmentEndingToday = assessments.Where(assessment => assessment.EndDate.Date == today).ToList();
+
+            foreach (var assessment in assessmentEndingToday)
+            {
+                var request = new NotificationRequest()
+                {
+                    NotificationId = assessment.Id + 6000,
+                    Title = "ASSESSMENT ENDING TODAY!",
+                    Description = $"Your assessment '{assessment.Name}' is ending today!",
+                    BadgeNumber = 1,
+                    CategoryType = NotificationCategoryType.Reminder,
+                    Schedule = new NotificationRequestSchedule()
+                    {
+                        NotifyTime = DateTime.Now.AddSeconds(5)
+                    }
+                };
+                await LocalNotificationCenter.Current.Show(request);
+            }
         }
     }
 }
